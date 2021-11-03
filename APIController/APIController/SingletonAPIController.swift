b0f5532aaca520c5f6559b103c2eff0e3e5b0414
Christophe Bugnon <christophe.bugnon@musicworldmedia.com>
2021-11-03 07:00:36 +0100

    Show how to test an API with a singleton (Subclass + property injection)

---
+++ APIController/APIController/SingletonAPIController.swift
@@ -0,0 +1,49 @@
+//
+//  SingletonAPIController.swift
+//  APIController
+//
+//  Created by Christophe Bugnon on 03/11/2021.
+//
+
+import Foundation
+
+enum Gender: Equatable {
+    case female
+    case male
+}
+
+struct Item: Equatable {
+    let name: String
+    let age: Int
+    let gender: Gender
+}
+
+class SingletonAPIClient {
+    typealias Result = Swift.Result<[Item], Error>
+    static let shared = SingletonAPIClient()
+
+    init() {}
+
+    func load(completion: @escaping (Result) -> Void) {}
+}
+
+protocol SingletonAPIRouter {
+    func display(_ error: Error)
+}
+
+class SingletonAPIController {
+    var api = SingletonAPIClient.shared
+    var router: SingletonAPIRouter?
+    var items: [Item]?
+
+    func load() {
+        api.load { [weak self] result in
+            switch result {
+            case let .success(feed):
+                self?.items = feed
+            case let .failure(error):
+                self?.router?.display(error)
+            }
+        }
+    }
+}
---
+++ APIController/APIControllerTests/SingletonAPIControllerTest.swift
@@ -0,0 +1,94 @@
+//
+//  SingletonAPIControllerTest.swift
+//  APIControllerTests
+//
+//  Created by Christophe Bugnon on 03/11/2021.
+//
+
+import Foundation
+import XCTest
+@testable import APIController
+
+class SingletonMockAPIClient: SingletonAPIClient {
+    var requestCallCount = 0
+    private var completions = [(Result) -> Void]()
+
+    override func load(completion: @escaping (Result) -> Void) {
+        requestCallCount += 1
+        completions.append(completion)
+    }
+
+    func complete(with feed: [Item], at index: Int = 0) {
+        completions[index](.success(feed))
+    }
+
+    func complete(with error: Error, at index: Int = 0) {
+        completions[index](.failure(error))
+    }
+}
+
+class SingletonAPIRouterSpy: SingletonAPIRouter {
+    var requestCallCount = 0
+
+    func display(_ error: Error) {
+        requestCallCount += 1
+    }
+}
+
+class SingletonAPIControllerTest: XCTestCase {
+    func test_init_doesNotRequestLoadFromClient() {
+        let (_, client) = makeSUT()
+
+        XCTAssertEqual(client.requestCallCount, 0)
+    }
+
+    func test_load_requestLoadFromClient() {
+        let (sut, client) = makeSUT()
+
+        sut.load()
+
+        XCTAssertEqual(client.requestCallCount, 1)
+    }
+
+    func test_load_deliversFeedOnClientSuccessFullRequest() {
+        let (sut, client) = makeSUT()
+        let feed = [Item(name: "Paul", age: 10, gender: .male),
+                    Item(name: "Huguette", age: 26, gender: .female)]
+
+        sut.load()
+        client.complete(with: feed)
+
+        XCTAssertEqual(sut.items, feed)
+    }
+
+    func test_load_doesNotDeliversFeedOnClientError() {
+        let (sut, client) = makeSUT()
+        let clientError = NSError(domain: "any error", code: 0)
+
+        sut.load()
+        client.complete(with: clientError)
+
+        XCTAssertNil(sut.items)
+    }
+
+    func test_load_requestRouterToDisplayErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        let clientError = NSError(domain: "any error", code: 0)
+        let router = SingletonAPIRouterSpy()
+        sut.router = router
+
+        sut.load()
+        client.complete(with: clientError)
+
+        XCTAssertEqual(router.requestCallCount, 1)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT() -> (sut: SingletonAPIController, client: SingletonMockAPIClient) {
+        let sut = SingletonAPIController()
+        let client = SingletonMockAPIClient()
+        sut.api = client
+        return (sut, client)
+    }
+}
